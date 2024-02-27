a2a13a861499100fe4e1ab0446169580f3c5ca3d
Yaël Dillies <yael.dillies@gmail.com>
2024-02-27 15:42:58 +0000

    Add `mk_all.sh` You should always use `./scripts/mk_all.sh; lake build` instead of a bare `lake build`. This is to make sure `LeanBourbaki.lean` imports all the files in the project.

--- LeanBourbaki.lean
+++ LeanBourbaki.lean
@@ -1,3 +1,1 @@
--- This module serves as the root of the `LeanBourbaki` library.
--- Import modules here that should be built as part of the library.
-import «LeanBourbaki».Chapter1.Chapter1Missing
+import LeanBourbaki.Chapter1
